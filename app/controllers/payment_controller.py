f35581f4ffa7f25b426d6c3f1afc0160531c6b66
Miguel Ângelo <miguelcg958@gmail.com>
2025-04-23 20:07:06 -0300

    add missing divission rest

--- app/controllers/payment_controller.py
+++ app/controllers/payment_controller.py
@@ -6,10 +6,13 @@ router = APIRouter(prefix="/payment", tags=["Payment"])
 def calculate(value: float, amount: int):
     installments = []
     installment_value = round(value / amount, 2)
+    installment_rest = value - (installment_value * amount)
 
-    for i in range(1, amount + 1):
+    for order_id in range(1, amount + 1):
+        installments.append({"order": order_id, "value": installment_value + installment_rest})
 
-        installments.append({"order": i, "value": installment_value})
+        if order_id == 1:
+            installment_rest = 0
 
     return {
         "value": value,
